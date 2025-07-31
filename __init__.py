8abdeb7b27de126874b92fb15eb8a3a8b5c16441
trashkollector <trashkollector@gmail.com>
2025-07-31 12:15:22 -0400

    initial checkin

---
+++ __init__.py
@@ -0,0 +1,38 @@
+from .nodes.gray_channel import ConvertGrayChannelNode
+from .nodes.brightness_contrast_saturation import AdjustBrightnessContrastSaturationNode
+from .nodes.baidu_translate import BaiduTranslateNode
+from .nodes.add_watermark import AddWaterMarkNode
+from .nodes.imitation_hue import ImitationHueNode
+from .nodes.light_shape import LightShapeNode
+from .nodes.highlight_shadow_brightness import HighlightShadowBrightnessNode
+from .nodes.hsl_color import HSLColorNode
+from .nodes.color_balance import ColorBalanceNode
+from .nodes.remove_watermark import RemoveWatermarkNode
+
+NODE_CLASS_MAPPINGS = {
+    "ConvertGrayChannelNode": ConvertGrayChannelNode,
+    "AdjustBrightnessContrastSaturationNode": AdjustBrightnessContrastSaturationNode,
+    "BaiduTranslateNode": BaiduTranslateNode,
+    "AddWaterMarkNode": AddWaterMarkNode,
+    "LightShapeNode": LightShapeNode,
+    "ImitationHueNode": ImitationHueNode,
+    "HighlightShadowBrightnessNode": HighlightShadowBrightnessNode,
+    "HSLColorNode": HSLColorNode,
+    "ColorBalanceNode": ColorBalanceNode,
+    "RemoveWatermarkNode": RemoveWatermarkNode,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ConvertGrayChannelNode": "Grayscale Channels",
+    "AdjustBrightnessContrastSaturationNode": "Brightness Contrast Saturation",
+    "BaiduTranslateNode": "Baidu Translate",
+    "AddWaterMarkNode": "Add Watermark",
+    "LightShapeNode": "IC-Light Light Shape",
+    "ImitationHueNode": "Imitation Hue",
+    "HighlightShadowBrightnessNode": "Highlight Shadow Brightness",
+    "HSLColorNode": "HSL Color",
+    "ColorBalanceNode": "Color Balance",
+    "RemoveWatermarkNode": "AI Remove Watermark",
+}
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
