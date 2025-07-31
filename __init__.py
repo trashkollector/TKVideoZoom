78396144bbf5266c73b8c43ad2e0605b1b038942
trashkollector <trashkollector@gmail.com>
2025-07-31 14:21:18 -0400

    fixed init

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# Import your node classes
+from .zoomcontrols import TKVideoZoom
+
+# Define NODE_CLASS_MAPPINGS
+NODE_CLASS_MAPPINGS = {
+    "TKVideoZoom": TKVideoZoom
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "TKVideoZoom": "TKVideoZoom"
+}
