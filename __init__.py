58230b8e6cf0992225e8455d1539f1504483ba1e
DESKTOP-HYTE\johnt <trashkollector@gmail.com>
2025-08-26 00:53:06 -0400

    added display mappings

--- __init__.py
+++ __init__.py
@@ -4,5 +4,14 @@ NODE_CLASS_MAPPINGS = {
     "TKVideoZoom": zoomcontrols.TKVideoZoom,
     "TKVideoSpeedZones": zoomcontrols.TKVideoSpeedZones,
     "TKVideoSmoothLooper": zoomcontrols.TKVideoSmoothLooper,
+    "TKVideoStitcher": zoomcontrols.TKVideoStitcher,
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+     "TKVideoZoom": "Various Video Effects, Zoom,Slide,Spin,etc.",
+     "TKVideoSpeedZones": "Slow down/speed up video in parts of video.",
+     "TKVideoSmoothLooper": "Loop a video .",
+     "TKVideoStitcher": "Stitch video clips together with transition.",
 
 }
